20e9da150aca0f04b10fdc6ebe6432ede7c504bd
Dannode36 <russelme848@gmail.com>
2023-01-12 13:58:32 +1100

    Reconnection and File Restructure
    v1.1.0
    Reconnection small changes
    Read the code comments
    Renaming Squash
    Update README.md

---
+++ RCONServerPlus/Exceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MinecraftServerRCON
+{
+    public class AuthException : Exception
+    {
+        public AuthException()
+        {
+        }
+
+        public AuthException(string message)
+            : base(message)
+        {
+        }
+
+        public AuthException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
--- RCONServerPlus/Properties/AssemblyInfo.cs
+++ RCONServerPlus/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MinecraftServerRCONPlus")]
+[assembly: AssemblyTitle("RCONServerPlus")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct("MinecraftServerRCONPlus")]
+[assembly: AssemblyProduct("RCONServerPlus")]
 [assembly: AssemblyCopyright("Copyright 2023")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion ("1.0.3")]
-[assembly: AssemblyFileVersion ("1.0.3")]
+[assembly: AssemblyVersion ("1.1.0")]
+[assembly: AssemblyFileVersion ("1.1.0")]
 [assembly: Guid ("e591b03e-9d64-44cc-b08d-2e6b85751ee7")]
 [assembly: AssemblyFlags (AssemblyNameFlags.PublicKey | AssemblyNameFlags.EnableJITcompileOptimizer)]
